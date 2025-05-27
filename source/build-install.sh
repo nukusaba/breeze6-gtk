c434e99bd97fe71ffcbc2cabb76e43393b5dc90c
mellowawa <may.src@proton.me>
2025-05-27 17:26:22 +0200

    Reapply "automated colorscheme stuff. and fixes"
    This reverts commit 0de0f97da12cf858fdef2b27c2c2e5688a5c4a24.

--- source/build-install.sh
+++ source/build-install.sh
@@ -9,8 +9,28 @@ fi
 
 theme_name="Breeze6-GTK"
 
+if [ "$1" == "" ]; then
+  echo ""
+  echo "Color options: --breeze-dark, --breeze-light, --hanokira, --sweet"
+  echo ""
+  exit 1
+fi
+# colors
+if [ "$1" == "--breeze-dark" ]; then
+  color="breeze-dark"
+elif [ "$1" == "--breeze-light" ]; then
+  color="breeze-light"
+elif [ "$1" == "--hanokira" ]; then
+  color="hanokira"
+elif [ "$1" == "--sweet" ]; then
+  color="sweet"
+elif [ "$1" == "--custom" ]; then
+  color="custom"
+fi
+mv colors/colors."$color".scss colors/colors.scss # this will be reset after the script runs
+
 # libadwaita
-if [ "$1" == "--libadwaita" ]; then
+if [ "$2" == "--libadwaita" ]; then
   mkdir -p ~/.config/gtk-4.0/
   rm -rf ~/.config/gtk-4.0/*.css
   sass gtk4-dark.scss ~/.config/gtk-4.0/gtk-dark.css
@@ -38,5 +58,9 @@ cp -r assets/ ~/.themes/"$theme_name"/gtk-4.0/
 # index
 cp index.theme ~/.themes/"$theme_name"/
 
+# reset
+mv colors/colors.scss colors/colors."$color".scss
+
 # exit
-echo "To install libadwata version, use --libadwaita argument."
+echo "To install libadwata version, add --libadwaita argument at the end."
+exit 0
